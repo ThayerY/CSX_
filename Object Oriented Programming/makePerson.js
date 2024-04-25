2945974293f5958cd8f2101dd6a54f37efd11ed1
ThayerY <thairyounussalim@gmail.com>
2024-04-25 20:16:24 +0300

    personStore

---
+++ Object Oriented Programming/makePerson.js
@@ -0,0 +1,27 @@
+/*
+ *Create a function that accepts two inputs (name and age) and returns an object.
+  Let's call this function makePerson. This function will:
+  1- Create an empty object
+  2- Add a name property to the newly created object with its value being
+  the name argument passed into the function
+  3- Add an age property to the newly created object with its value being
+  the "age" argument passed into the function
+  4- Return the object 
+ */
+
+function makePerson(name, age) {
+  // add code here
+  const obj = {
+    name: name,
+    age: age
+  }
+  return obj
+
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
